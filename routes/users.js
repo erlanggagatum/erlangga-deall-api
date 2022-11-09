405dde2f0601eb85cd01479b048193ef7a60d5a2
erlanggagatum <erlanggagatum@gmail.com>
2022-11-09 12:07:43 +0700

    add complete rest api example with express and prisma

--- routes/users.js
+++ routes/users.js
@@ -6,32 +6,81 @@ const { PrismaClient, Prisma } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 
-
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
   // get all user
   const users = await prisma.user.findMany()
-  res.status(200).json(users);
+  res.status(200).json({status:"OK", data: users});
+});
+
+
+/* GET users listing. */
+router.get('/:id', async (req, res, next) => {
+  // get all user
+  const user = await prisma.user.findFirstOrThrow({
+    where: {
+      id: parseInt(req.params.id)
+    }
+  })
+  res.status(200).json({status:"OK", data: user});
 
 });
 
+// POST User
 router.post("/", async (req, res) => {
-  // generate random user
-  const user = await prisma.user.create({
-    data: {
-      email: "gtm2@mail.com",
-      name: "gatum2",
-      password: "mypassword2",
-      posts: { 
-        create: {
-          title: "my first post",
-          content: "wow, this is great!"
-        } 
-      }
-    }
-  })
+  
+  // Get user data from POST request
+  const { user } = req.body
+  console.log(user)
+  
+  try {
 
-  res.status(200).json(user)
+    if (user.length > 1) {
+      var userCreate = await prisma.user.createMany({
+        data: user
+      })
+    } else {
+      var userCreate = await prisma.user.create({
+        data: user
+      })
+    }
+    
+    res.status(200).json({user: user, userCreate: userCreate})
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+// PUT / update user data
+// also working with PATCH method
+router.put('/:id', async (req, res) => {
+  const user = req.body
+  console.log(user, req.params.id)
+  try {
+    const userUpdate = await prisma.user.update({
+      where: {
+        id: parseInt(req.params.id)
+      },
+      data: user
+    })
+    res.status(200).json({user: user, userUpdate: userUpdate})
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+// DELETE user data
+router.delete("/:id", async (req, res) => {
+  try {
+    const userDelete = await prisma.user.delete({
+      where: {
+        id: parseInt(req.params.id)
+      }
+    })
+    res.status(200).json({userDelete: userDelete})
+  } catch (error) {
+    res.status(500).json(error)
+  }
 })
 
 module.exports = router;
